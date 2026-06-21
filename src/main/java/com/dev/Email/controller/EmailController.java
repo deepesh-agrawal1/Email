5a834e12902fafd3042bb24744eeb658f645b193
deepesh-agrawal1 <adevagrawal@gmail.com>
2026-06-21 13:33:15 +0530

    hi

--- src/main/java/com/dev/Email/controller/EmailController.java
+++ src/main/java/com/dev/Email/controller/EmailController.java
@@ -1,11 +1,9 @@
 package com.dev.Email.controller;
 
 import com.dev.Email.service.EmailService;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/email")
@@ -14,15 +12,33 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    @PostMapping("/send")
-    public String sendEmail(
-            @RequestParam String to) {
 
-        emailService.sendEmail(
-                to,
-                "Test Mail",
-                "Hello from Spring Boot");
+    @GetMapping("/send-pdf")
+    public String sendPdf(
+            @RequestParam String to)
+            throws MessagingException {
 
-        return "Email Sent";
+        emailService.sendEmail(to,
+                "Graduate Seeking Software Engineering Opportunities | Ex- Intern HSBC",
+                "Hi,\n" +
+                        "\n" +
+                        "I hope you are doing well.\n" +
+                        "\n" +
+                        "My name is Deepesh Agrawal, and I recently completed my B.Tech in Computer Science and Design.\n" +
+                        "\n" +
+                        "I am reaching out to express my interest in Software Engineering opportunities at Mastercard. During my internship, I worked on backend development using Java, Spring Boot, REST APIs, PostgreSQL, Git, Jenkins, and Ansible. I also gained experience working in an enterprise development environment and contributing to production-oriented projects.\n" +
+                        "\n" +
+                        "I am particularly interested in entry-level Software Engineering and technology-focused roles where I can contribute while continuing to learn and grow.\n" +
+                        "\n" +
+                        "I have attached my resume for your review. If there are any suitable openings that match my profile, I would be grateful for an opportunity to be considered.\n" +
+                        "\n" +
+                        "Thank you for your time and consideration. I look forward to hearing from you.\n" +
+                        "\n" +
+                        "Best Regards,\n" +
+                        "Deepesh Agrawal\n" +
+                        "Phone: 9527034785\n" +
+                        "Email:");
+
+        return "PDF Email Sent Successfully";
     }
 }
--- src/main/java/com/dev/Email/service/EmailService.java
+++ src/main/java/com/dev/Email/service/EmailService.java
@@ -1,11 +1,13 @@
 package com.dev.Email.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import java.io.File;
 
 @Service
 public class EmailService {
@@ -14,17 +16,22 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendEmail(
-            String to,
-            String subject,
-            String body) {
+    public void sendEmail(String to,String subject,String body) throws MessagingException {
 
-        SimpleMailMessage message =
-                new SimpleMailMessage();
+        MimeMessage message = mailSender.createMimeMessage();
 
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
+        MimeMessageHelper helper =
+                new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(body);
+
+        FileSystemResource file =
+                new FileSystemResource(
+                        new File("C:/Users/Sai/Project Interview/Email/resume.pdf"));
+
+        helper.addAttachment("Deepesh_agrawal-Resume.pdf", file);
 
         mailSender.send(message);
     }
